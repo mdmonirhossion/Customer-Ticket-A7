8bfbc6466f2341a030ed4d77930753cf40b629fd
Monir Hossion <mdmonirhossion2002@gmail.com>
2025-12-09 22:43:59 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import React, { Suspense } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import  TicketSystem from './Components/TicketSystem/TicketSystem'
+import Footer from './components/Footer/Footer'
+import { ToastContainer } from 'react-toastify'
+import Loader from './components/Loader/Loader'
 
-import './App.css'
+const ticketData = async () => {
+  const res = await fetch('/ticketData.json')
+  return res.json()
+}
+
+const tiketDataPromise = ticketData()
 
 const App = () => {
-  
-
   return (
     <>
-     
-      
-      
-      
-     
+    <ToastContainer /> 
+      <Navbar />
+      <Suspense fallback={<Loader />}>
+      <TicketSystem tiketDataPromise={tiketDataPromise} />
+      </Suspense>
+      <Footer />
     </>
   )
 }
